6805b979bf38bd843b2718e99567d7f7c27f815d
Mythireyan <mythireyanp@gmail.com>
2020-10-14 12:38:54 +0530

    Menu-Items routing are done based on HOC

--- src/Components/ContentBackground/ContentBackground.js
+++ src/Components/ContentBackground/ContentBackground.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import {withRouter} from 'react-router-dom';
 
 import './ContentBackground.scss';
 
-function ContentBackground({title, imageUrl, size}){
+function ContentBackground({title, imageUrl, size, history, linkUrl, match}){
 	return(
-				<div className={`${size} content__background`}>   
+			 	<div className={`${size} content__background`} onClick={()=> history.push(`${match.url}${linkUrl}`)}>   
 					<div className= 'backgroundImage' style ={{
 						backgroundImage:`url(${imageUrl})`
 					}}/>
@@ -17,4 +18,4 @@ function ContentBackground({title, imageUrl, size}){
 		)
 }
 
-export default ContentBackground;
+export default withRouter(ContentBackground);
