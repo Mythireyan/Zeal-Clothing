982db7ec90d742ef80c8d8e8b2153ba06ae90b09
Mythireyan <mythireyanp@gmail.com>
2020-10-21 12:23:23 +0530

    Redirecting to shop page when user signs in

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import {Route, Switch} from 'react-router-dom';
+import {Route, Switch, Redirect} from 'react-router-dom';
 import HomePage from './Pages/HomePage/HomePage';
 import ShopPage from './Pages/ShopPage/ShopPage';
 import Header from './Components/Header/Header';
@@ -44,15 +44,24 @@ class  App extends React.Component{
     <Switch>
       <Route exact path='/' component={HomePage}/>
       <Route path='/shop' component={ShopPage}/>
-      <Route path='/signIn' component={SignInSignUpPage}/>
+      <Route exact
+             path='/signIn' 
+             render={()=>this.props.currentUser 
+              ? (<Redirect to='/'/>) 
+              : (<SignInSignUpPage/>)}
+        />
     </Switch>  
     </div>
   );
   }  
 }
 
+const mapStateToProps = ({user})=>({
+  currentUser:user.currentUser,
+})
+
 const mapDispatchToProps= (dispatch)=>({
   setCurrentUser : user => dispatch(setCurrentUser(user))
 });
 
-export default connect(null, mapDispatchToProps)(App);
+export default connect(mapStateToProps, mapDispatchToProps)(App);
