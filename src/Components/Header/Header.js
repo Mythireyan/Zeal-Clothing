6a2ce125434caf240a2aa503d699258bf001eae4
Mythireyan <mythireyanp@gmail.com>
2020-10-22 14:46:25 +0530

    added add to cart functionality and quantity of product functionality

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -38,7 +38,6 @@ const Header = ({currentUser, hidden})=>(
 const mapStateToProps = ({user:{currentUser}, cart:{hidden}})=>({
 	currentUser,
 	hidden
-
 });
 
 export default connect(mapStateToProps)(Header);
--- src/Redux/Cart/CartAction.js
+++ src/Redux/Cart/CartAction.js
@@ -3,3 +3,10 @@ import {CartActionTypes} from './CartTypes';
 export const toggleClassHidden = ()=>({
 	type:CartActionTypes.TOGGLE_CART_HIDDEN,
 })
+
+export const addItem = (item)=>{
+	return{
+		type:CartActionTypes.ADD_ITEM,
+		payload:item,
+	}
+}
